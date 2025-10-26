037066d32d7a3688115f52828cd4874d7d8da3f8
Michał Piechota <michalpiechotaa13@gmail.com>
2025-10-26 11:16:51 +0100

    Fix transform for greyscale images.

--- src/image_processing.py
+++ src/image_processing.py
@@ -10,6 +10,21 @@ import torchvision
 _MEAN = (0.485, 0.456, 0.406)
 _STD = (0.229, 0.224, 0.225)
 
+
+class ConditionalNormalize(object):
+    def __init__(self, mean, std):
+        self.normalize = torchvision.transforms.Normalize(mean=mean, std=std)
+
+    def __call__(self, tensor):
+        """
+        Skip normalization if called on greyscale image and enforce 3 channels.
+        """
+        if tensor.shape[0] == 3:
+            return self.normalize(tensor)
+        else:
+            return torch.concat((tensor, tensor, tensor), 0)
+
+
 _TRANSFORMS = immutabledict.immutabledict(
     {
         "resnet18": torchvision.transforms.Compose(
@@ -17,7 +32,7 @@ _TRANSFORMS = immutabledict.immutabledict(
                 torchvision.transforms.Resize(224),
                 torchvision.transforms.CenterCrop((224, 224)),
                 torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize(mean=_MEAN, std=_STD),
+                ConditionalNormalize(_MEAN, _STD),
             ]
         )
     }
@@ -49,4 +64,4 @@ def save_images_from_iteration(
     for image_number, image in enumerate(images):
         filename = f"{run_id}_{iteration}_{image_number}"
         save_path = f"{directory_path}/{filename}"
-        image.save(save_path, format="jpeg")    
+        image.save(save_path, format="jpeg")
