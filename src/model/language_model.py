2149993b1ad3aa782316c4fca7c056c33a4b175c
GasparSekula <gasp.gspr@gmail.com>
2025-10-29 22:41:42 +0100

    Update language model

--- src/model/language_model.py
+++ src/model/language_model.py
@@ -3,9 +3,52 @@ import transformers
 
 from model import model
 import prompt_utils
+from collections.abc import Mapping
+import random
+
 
 _ASSISTANT_TAG = "<|assistant|>"
+_RANDOM_SEED = 42
+_MAX_CONCEPT_DICT_SIZE = 100
 
+def select_concepts(concepts_dict: Mapping[str, float], 
+                    new_concept: str, 
+                    new_concept_score: float,
+                    p: float = 0.9) -> Mapping[str, float]:
+    
+    if not concepts_dict:
+        return {new_concept: new_concept_score}
+    
+    if not (new_concept and new_concept_score):
+        return concepts_dict
+    
+    worst_concept = min(concepts_dict, key=concepts_dict.get)
+    
+    rng = random.Random(_RANDOM_SEED)
+    
+    new_dict = dict(concepts_dict)
+
+    if new_concept in new_dict:
+        new_dict[new_concept] = max(new_dict[new_concept], new_concept_score)
+        return new_dict
+
+    if len(new_dict) < _MAX_CONCEPT_DICT_SIZE:
+        new_dict[new_concept] = new_concept_score
+        return new_dict
+
+    if new_concept_score > new_dict[worst_concept]:
+        del new_dict[worst_concept]
+        new_dict[new_concept] = new_concept_score
+        return new_dict
+
+    if rng.random() > p:
+        replace_key = rng.choice(list(new_dict.keys()))
+        del new_dict[replace_key]
+        new_dict[new_concept] = new_concept_score
+
+    return new_dict
+    
+        
 
 class LanguageModel(model.Model):
     def __init__(
@@ -43,13 +86,29 @@ class LanguageModel(model.Model):
         # sample random imagenet classes, score them and create a dict
         # best: {concept: score}
         # random: {concept: score}
-        self._concept_history = dict()
+        
+        # hardcoded for testing purposes 
+        # TODO: implement initialization
+        
+        best = {"cup" : 0.9, "cafe": 0.86, "tea" : 0.78, "juice": 0.71, "hot": 0.58}
+        random = {"road": 0.32, "beer": 0.71, "horse": 0.22, "cake": 0.49, "toothpaste": 0.13}
+        concept_dict = {**best, **random}
+        
+        self._concept_history = dict(concept_dict)
+
 
     def _update_concept_history(
         self, new_concept: str, auc_score: float
     ) -> None:
         """Updates concept history with most recent concept."""
-        pass
+        new_concept_dict = select_concepts(
+            self._concept_history,
+            new_concept,
+            auc_score
+        )
+        
+        self._concept_history = new_concept_dict
+        
 
     def generate_concept(self):
         """Generates new concept based on concept history."""
--- src/prompt_utils.py
+++ src/prompt_utils.py
@@ -9,27 +9,21 @@ def concept_image_prompt(prompt_text: str, concept: str) -> str:
 
 def generate_concept_prompt(concept_history: Mapping[str, float]) -> str:
     # this will require some prompt engineering (xd), temp version
-    # hardcoded for testing
-    best_list = "stripes: 0.85, animal: 0.67"
-    random_list = "fish: 0.45, truck: 0.34"
+    concept_list = "; ".join(f"{k}: {v}" for k, v in concept_history.items()) + "; "
     template = """
     Propose a new concept based on the concept history. Each concept is scored 
-    with a value from 0 to 1, the bigger the better. The history contain best 
-    scored concepts inside <BEST>...</BEST> tags and random concepts inside 
-    <RANDOM>...</RANDOM> tags. The format is concept: score, e.g. white: 0.52. 
+    with a value from 0 to 1, the bigger the better. The history contain 
+    scored concepts inside <CONCEPTS>...</CONCEPTS> tags. The format is concept: score, e.g. white: 0.52. 
     Try to infer new concept instead of reusing the ones listed here.
 
-    <BEST>
-    {best_list}
-    </BEST>
-    <RANDOM>
-    {random_list}
-    </RANDOM>
+    <CONCEPTS>
+    {concept_list}
+    </CONCEPTS>
     
     Answer only with the concept.
     """
 
-    return template.format(best_list=best_list, random_list=random_list).strip()
+    return template.format(concept_list=concept_list).strip()
 
 
 if __name__ == "__main__":
