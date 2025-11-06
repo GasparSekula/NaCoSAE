95c704507b593ab97e05108e13ef2d9d9e719ddb
Michał Piechota <68874244+piechotam@users.noreply.github.com>
2025-11-06 20:59:07 +0100

    Fix formatting of prompt path definition

--- src/main.py
+++ src/main.py
@@ -34,7 +34,9 @@ _METRIC = flags.DEFINE_enum_class(
 )
 
 _PROMPT_PATH = flags.DEFINE_string(
-    "prompt", "src/prompts/templates/prompt_mils.txt", "Path to prompt for the LLM."
+    "prompt",
+    "src/prompts/templates/prompt_mils.txt",
+    "Path to prompt for the LLM.",
 )
 
 
