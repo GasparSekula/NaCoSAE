07b3e2a980d53c7a307422d29ac17cdbf3820bf3
GasparSekula <gasp.gspr@gmail.com>
2025-11-19 08:24:40 +0100

    Initial chain of thought prompting implementation.

--- src/main_test_lm.py
+++ src/main_test_lm.py
@@ -6,7 +6,7 @@ def main():
         model_id="meta-llama/Llama-3.1-8B-Instruct",
         device="cuda",
         max_new_tokens=30,
-        prompt_path="/src/prompts/templates/prompt_few_shot.txt",
+        prompt_path="/src/prompts/templates/cot/prompt_cot_few_shot.txt",
     )
 
     concepts = {
@@ -25,16 +25,20 @@ def main():
 
     print(f"\n Concept history:\n {lm._concept_history}")
 
-    print("\n\n Starting concept geenration loop")
+    print("\n\n Starting concept generation loop")
 
-    for i in range(3):
-        concept = lm.generate_concept()
-        print(
-            f"\nLoop {i+1} of 3. Current generation history:\n {lm.generation_history}"
-        )
-        lm.update_concept_history(new_concept=concept, score=0.8)
+    # for i in range(3):
+    #     concept = lm.generate_concept()
+    #     print(
+    #         f"\nLoop {i+1} of 3. Current generation history:\n {lm.generation_history}"
+    #     )
+    #     lm.update_concept_history(new_concept=concept, score=0.8)
 
-    print(f"\n\n Final concept history:\n {lm._concept_history}")
+    prompt = lm.generate_concept()
+    
+    print(f"\n\n Prompt: {prompt}")
+
+    # print(f"\n\n Final concept history:\n {lm._concept_history}")
 
 
 if __name__ == "__main__":
