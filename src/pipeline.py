9daf8c8e8f6924eb423570caceb5297a8d9fdc77
GasparSekula <126190938+GasparSekula@users.noreply.github.com>
2025-11-20 17:37:30 +0100

    Best concepts history. (#25)
    * Add saving best cocnepts.
    
    * Saving best concepts to file.
    
    * Syntax fixes in best concept history update.
    
    * Formatting fixes.
    
    * Move format_concept_history function to history_managing.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -47,6 +47,8 @@ class Pipeline:
             f"{neuron_id}"
         )
 
+        self._best_concepts = []
+
         self._save_directory = os.path.join(
             self._history_managing_config.save_directory, self._run_id
         )
@@ -158,6 +160,15 @@ class Pipeline:
         score = self._score_concept(new_concept, concept_synthetic_images)
         self._lang_model.update_concept_history(new_concept, score)
 
+        best_concept, best_score = self._lang_model.get_best_concept()
+        self._best_concepts.append(
+            {
+                "iteration": iter_number,
+                "best_concept": best_concept,
+                "best_score": best_score,
+            }
+        )
+
         return new_concept, score
 
     def _initialize_concept_history(self) -> Mapping[str, float]:
@@ -184,7 +195,7 @@ class Pipeline:
             "generation_history.txt",
         )
         history_managing.save_llm_history(
-            concept_history.format_concept_history(
+            history_managing.format_concept_history(
                 self._lang_model.concept_history
             ),
             self._save_directory,
@@ -217,4 +228,11 @@ class Pipeline:
         logging.info(
             "BEST CONCEPT FOUND: %s with score %f."
             % (self._lang_model.get_best_concept())
+        )
+
+        logging.info("SAVING BEST CONCEPTS.")
+        history_managing.save_llm_history(
+            history_managing.format_best_concepts_history(self._best_concepts),
+            self._save_directory,
+            "best_concepts.txt",
         )
