fb1308645c4480cb653dad476cd921456efc0e70
GasparSekula <gasp.gspr@gmail.com>
2025-12-04 15:25:42 +0100

    Streamlit application 2nd version

--- app/components/plots.py
+++ app/components/plots.py
@@ -1,12 +1,76 @@
 from typing import Sequence, Tuple
 
+import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+
+COLOR = "red"
+
+
+def plot_design_setup() -> None:
+    plt.rcParams["figure.figsize"] = (16, 9)
+    plt.rcParams["figure.facecolor"] = "none"
+    plt.rcParams["axes.facecolor"] = "none"
+    plt.rcParams["savefig.facecolor"] = "none"
+    plt.rcParams["text.color"] = "white"
+    plt.rcParams["axes.labelcolor"] = "white"
+    plt.rcParams["axes.titlecolor"] = "white"
+    plt.rcParams["xtick.color"] = "white"
+    plt.rcParams["ytick.color"] = "white"
+    plt.rcParams["font.size"] = 20
 
 
 def plot_score_vs_iteration(
     generation_history_scores: Sequence[float],
 ) -> None:
+    scores_array = np.array(generation_history_scores, dtype=np.float32)
+    x_ticks = range(1, len(scores_array) + 1)
+
     fig, ax = plt.subplots()
-    ax.plot(generation_history_scores)
+    ax.set_title("Score vs iteration")
+    ax.plot(x_ticks, scores_array, color=COLOR)
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Score")
+    ax.set_ylim(bottom=0.9 * min(scores_array), top=1.1 * max(scores_array))
+    ax.set_xticks(x_ticks)
+    ax.grid(True)
+    st.pyplot(fig)
+
+
+def plot_relative_score_over_iteration(
+    generation_history_scores: Sequence[float],
+) -> None:
+    scores_array = np.array(generation_history_scores, dtype=np.float32)
+    best_score = max(scores_array)
+    relative_scores_array = scores_array / best_score
+    x_ticks = range(1, len(relative_scores_array) + 1)
+
+    fig, ax = plt.subplots()
+    ax.set_title("Relative score vs iteration")
+    ax.plot(x_ticks, relative_scores_array, color=COLOR)
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Relative score")
+    ax.set_ylim(bottom=0.9 * min(relative_scores_array), top=1.1)
+    ax.set_xticks(x_ticks)
+    ax.grid(True)
+    st.pyplot(fig)
+
+
+def plot_best_score_over_iteration(
+    generation_history_scores: Sequence[float],
+) -> None:
+    scores_array = np.array(generation_history_scores, dtype=np.float32)
+    cumulative_best_score = np.maximum.accumulate(scores_array)
+    x_ticks = range(1, len(scores_array) + 1)
+
+    fig, ax = plt.subplots()
+    ax.set_title("Cumulative best score vs iteration")
+    ax.plot(x_ticks, cumulative_best_score, color=COLOR)
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Relative score")
+    ax.set_ylim(
+        bottom=0.9 * min(cumulative_best_score), top=1.1 * max(scores_array)
+    )
+    ax.set_xticks(x_ticks)
+    ax.grid(True)
     st.pyplot(fig)
--- app/pages/dashboard.py
+++ app/pages/dashboard.py
@@ -10,4 +10,77 @@ experiment_results: results.ExperimentResults = st.session_state.get(
 generation_history_scores = history_processing.get_scores_from_history(
     experiment_results.generation_history
 )
-plots.plot_score_vs_iteration(generation_history_scores)
+
+st.write("# Analyze model output")
+
+st.write("## Parameters")
+st.write(
+    f"**Explained model:** {str(experiment_results.run_params["load_config"]["explained_model_id"])}"
+)
+st.write(
+    f"- Layer: {str(experiment_results.run_params["model_layer_activations_path"].split("/")[-1])}"
+)
+st.write(f"- Neuron: {str(experiment_results.run_params["neuron_id"])}")
+st.write(
+    f"**Language Model:** {str(experiment_results.run_params["load_config"]["language_model_id"])}"
+)
+st.write(
+    f"**Text-To-Image Model:** {str(experiment_results.run_params["load_config"]["text_to_image_model_id"])}"
+)
+st.write(
+    f"**Number of iterations:** {len(experiment_results.generation_history) - 1} + 1 summary run"
+)
+
+
+st.write("## Generated concepts")
+st.dataframe(
+    {
+        "Iteration": range(1, len(experiment_results.generation_history) + 1),
+        "Generated Concept": [
+            concept[0] for concept in experiment_results.generation_history
+        ],
+        "Score": [
+            round(float(concept[1]), 2)
+            for concept in experiment_results.generation_history
+        ],
+    },
+    hide_index=True,
+)
+
+st.write("## Final concepts set")
+st.write("Includes best concepts among initial ones and generated ones.")
+st.dataframe(
+    {
+        "Iteration": range(
+            1, len(experiment_results.final_concept_history) + 1
+        ),
+        "Generated Concept": [
+            concept[0] for concept in experiment_results.final_concept_history
+        ],
+        "Score": [
+            round(float(concept[1]), 2)
+            for concept in experiment_results.final_concept_history
+        ],
+    },
+    hide_index=True,
+)
+
+
+st.write(" ")
+plots.plot_design_setup()
+
+plot_option = st.selectbox(
+    "Visualize results",
+    [
+        "Score vs Iteration",
+        "Relative Score vs Iteration",
+        "Cumulative Best Score vs Iteration",
+    ],
+    placeholder="Select plot...",
+)
+if plot_option == "Score vs Iteration":
+    plots.plot_score_vs_iteration(generation_history_scores)
+elif plot_option == "Relative Score vs Iteration":
+    plots.plot_relative_score_over_iteration(generation_history_scores)
+elif plot_option == "Cumulative Best Score vs Iteration":
+    plots.plot_best_score_over_iteration(generation_history_scores)
